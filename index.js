2a136b1952eb54a653f39af839ce0be159d4aa87
akilinjamam <akilinjamam@gmail.comm>
2022-05-17 20:37:55 +0600

    file-updated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const res = require('express/lib/response');
 const app = express()
 require('dotenv').config();
-const port = process.env.port || 5000
+const port = process.env.PORT || 5000
 
 // middle ware
 
