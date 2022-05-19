7edb004632219c7268c3288d9d7844210be541d6
akilinjamam <akilinjamam@gmail.comm>
2022-05-19 09:49:06 +0600

    file-updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
-// const res = require('express/lib/response');
+const res = require('express/lib/response');
 const app = express()
 require('dotenv').config();
 const port = process.env.PORT || 5000
